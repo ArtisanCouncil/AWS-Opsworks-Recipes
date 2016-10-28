bfb8d9f5f92a5e640a3f766c529f78cd6b85e9cf
danwelsh <chaos@chaos-MB.local>
2016-10-28 17:06:13 +1100

    changed composer user and command

--- ac-deploy/definitions/laravel.rb
+++ ac-deploy/definitions/laravel.rb
@@ -57,9 +57,9 @@ Chef::Log.info "AC-DEPLOY: run composer release path #{release_path}/src"
         # run install with flags generated above 
         Chef::Log.info "Running composer update with flags #{update_flags}"
         execute "composer update" do
-	        #user "deploy" # "#{node[:deploy][application][:user_name]}" #added to ensure correct user
+	        user "deploy" # "#{node[:deploy][application][:user_name]}" #added to ensure correct user
             cwd "#{release_path}/src" # TOD
-            command "composer #{update_flags} update"
+            command "sudo composer #{update_flags} update"
             ignore_failure false
         end
 
@@ -67,7 +67,7 @@ Chef::Log.info "AC-DEPLOY: run composer release path #{release_path}/src"
         execute "composer dump-autoload" do
 	        user "deploy" # "#{node[:deploy][application][:user_name]}" #added to ensure correct user
             cwd "#{release_path}/src" # TOD
-            command "composer dump-autoload"
+            command "sudo composer dump-autoload"
             ignore_failure false
         end
 
